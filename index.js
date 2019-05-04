b36ec6d1c0c931fc6ee06b9207571586375d771c
Bachecafet <bacheca.1710194@studenti.uniroma1.it>
2019-05-04 16:51:43 +0200

    update

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express');
 //set up express
 const app = express();
 
-app.get('/', function(req,res){
+app.get('/api', function(req,res){
 
     console.log('GET request');
-    res.end();
+    res.send({name: 'Yoshi'});
 
 });
 
