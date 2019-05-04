3bdc27c3e17fec153408bd300dd4a1ebdc58bfb2
Bachecafet <bacheca.1710194@studenti.uniroma1.it>
2019-05-04 12:47:48 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+//set up express
+const app = express();
+
+//listen for request
+
+app.listen(process.env.port || 4000,function(){
+    console.log('Listening...\n');
+});
