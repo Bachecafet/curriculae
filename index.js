e62b0d9ab2fe2b410e705a07cdd357ad3d2f9501
Bachecafet <bacheca.1710194@studenti.uniroma1.it>
2019-05-04 16:39:21 +0200

    cosocosetto

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express');
 //set up express
 const app = express();
 
+app.get('/', function(req,res){
+
+    console.log('GET request');
+    res.end();
+
+});
+
 //listen for request
 app.listen(process.env.port || 4000,function(){
     console.log('Listening...\n');
