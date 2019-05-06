6b3bcf54cb9edb96feddc0d4a526932fb60004d4
Bachecafet <bacheca.1710194@studenti.uniroma1.it>
2019-05-06 11:55:26 +0200

    òm2

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-const express = require('express');
-const routes= require('./routers/api');
-const bodyParser = require('body-parser');
-var path = require('path');
-var port = process.env.port || 4000;
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-
-//set up express
-const app = express();
-app.set('view engine', 'html');
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,'views')));
-app.use('/',routes);
-
-
-//error handling
-app.use(function(err,req,res,next){
-    console.log('err');
-    res.status(422).send({error: err.message});
-});
-
-//listen for request
-app.listen(port,function(){
-    console.log('Listening on port: ' + port);
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
